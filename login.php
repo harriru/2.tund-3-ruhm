2950f0dab349e4a39092bc56cc7b96d2af2a15c2
Harri Ruttas <harrrut@greeny.cs.tlu.ee>
2015-09-09 12:38:17 +0300

    kodutöö login page

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+
+	// LOGIN.PHP
+	
+
+?>
+<html>
+<head>
+<title>Login page</title>
+</head>
+
+<body>
+	<h2>log in</h2>
+	
+		<input type="email" placeholder = "email"> <br><br>
+		<input type="password" placeholder = "parool"> <br><br>
+		<input type="submit" value="login">
+
+
+
+	<h2>create user</h2>
+</body>
+
+
+</html>
