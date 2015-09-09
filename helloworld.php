7dbf039985654b2ed78e1f686484f1b75dd6f906
Harri Ruttas <harrrut@greeny.cs.tlu.ee>
2015-09-09 11:34:53 +0300

    first looog

--- helloworld.php
+++ helloworld.php
@@ -3,3 +3,21 @@
 	$last_name = "Ruttas";
 	echo $first_name." ".$last_name;
 ?>
+<br>
+<?php
+
+	$age = 5;
+
+	//testib loogikat, juhul kui vanus on väiksem 
+	//kui 18 siis kirjuta "alaealine" , muul juhul "täisaealine" 
+
+	if($age < 18) {
+		//tõene
+		echo "alaealine";
+	} 
+	else {
+		//väär
+		echo "täisaealine";
+	}
+
+?>
