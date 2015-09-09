a7c59e8259934eeea5e1763c0e92f433724638a0
Harri Ruttas <harrrut@greeny.cs.tlu.ee>
2015-09-09 13:30:17 +0300

    login page

--- login.php
+++ login.php
@@ -3,6 +3,7 @@
 	// LOGIN.PHP
 	// echo $_POST["email"];
 	$email_error = "";
+	$password_error = "";
 
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
 		
@@ -11,8 +12,20 @@
 		if (empty($_POST["email"]) )  {
 			$email_error = "see väli on kohustuslik";
 
-		}
+	
 	}
+
+		if (empty($_POST["password"]) )  {
+			$password_error = "see väli on kohustuslik";
+		} else {
+			//siis pole parool tyhi
+			if(strlen($_POST["password"]) < 8)	{
+				$password_error = "Peab olema vähemalt 8 tähemärki pikk!";
+			}
+		}			
+
+}
+
 ?>
 <html>
 <head>
@@ -24,7 +37,7 @@
 		
 		<form action="login.php" method="post">
 		<input name="email"e type="email" placeholder = "email"> <?php echo $email_error;  ?><br><br>
-		<input name="password" type="password" placeholder = "parool"> <br><br>
+		<input name="password" type="password" placeholder = "parool"> <?php echo $password_error;  ?><br><br>
 		<input type="submit" value="login">
 		</form>
 
