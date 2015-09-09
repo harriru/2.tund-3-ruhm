6770cda78a5cb3bde0e2211300e57e1f3c0a0e7a
Harri Ruttas <harrrut@greeny.cs.tlu.ee>
2015-09-09 13:08:41 +0300

    email error

--- login.php
+++ login.php
@@ -1,8 +1,18 @@
 <?php
 
 	// LOGIN.PHP
-	
+	// echo $_POST["email"];
+	$email_error = "";
 
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		
+		//echo "keegi vajutas nuppu";
+		
+		if (empty($_POST["email"]) )  {
+			$email_error = "see väli on kohustuslik";
+
+		}
+	}
 ?>
 <html>
 <head>
@@ -11,11 +21,12 @@
 
 <body>
 	<h2>log in</h2>
-	
-		<input type="email" placeholder = "email"> <br><br>
-		<input type="password" placeholder = "parool"> <br><br>
+		
+		<form action="login.php" method="post">
+		<input name="email"e type="email" placeholder = "email"> <?php echo $email_error;  ?><br><br>
+		<input name="password" type="password" placeholder = "parool"> <br><br>
 		<input type="submit" value="login">
-
+		</form>
 
 
 	<h2>create user</h2>
